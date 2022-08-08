99029689af978d449492f991aba0b89037516afa
= <3lian3.pereira@gmail.com>
2022-08-08 20:07:58 -0300

    corrigindo bug desafio3

---
+++ dist/desafio1.js
@@ -0,0 +1,12 @@
+"use strict";
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+//Respostas
+let employee = {
+    codeEmployee: 10,
+    nameEmployee: "John"
+};
+let funcionario = {
+    code: 20,
+    name: "eliane"
+};
+//# sourceMappingURL=desafio1.js.map
--- dist/desafio3.js
+++ dist/desafio3.js
@@ -1,48 +1,40 @@
+"use strict";
 // export {}
 // O código abaixo tem alguns erros e não funciona como deveria. Você pode identificar quais são e corrigi-los em um arquivo TS?
-
-let botaoAtualizar = document.getElementById('atualizar-saldo') as HTMLButtonElement;
-let botaoLimpar = document.getElementById('limpar-saldo') as HTMLButtonElement;
-let soma = document.getElementById('soma') as HTMLInputElement;
-let campoSaldo = document.getElementById('campo-saldo') as HTMLSpanElement;
-
-
-let saldoTotal = 0
-
-limparSaldo()
-
-function somarAoSaldo(soma: number) {
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+let saldoTotal = 0;
+limparSaldo();
+function somarAoSaldo(soma) {
     if (campoSaldo) {
-        saldoTotal += soma
+        saldoTotal += soma;
         campoSaldo.innerHTML = saldoTotal.toString();
         limparCampoSoma();
     }
 }
-
 function limparCampoSoma() {
     soma.value = "";
 }
-
 function limparSaldo() {
     if (campoSaldo) {
         saldoTotal = 0;
         campoSaldo.innerHTML = saldoTotal.toString();
     }
 }
-
 if (botaoAtualizar) {
     botaoAtualizar.addEventListener('click', () => {
-        somarAoSaldo(Number(soma.value)); 
+        somarAoSaldo(Number(soma.value));
     });
 }
-botaoLimpar.addEventListener('click', () => { // Percebam que aqui o typescript não acusou o botao de ser nulo e não precisei do if. Caso queiram fazer o teste, retirem a exclamação.
+botaoLimpar.addEventListener('click', () => {
     limparSaldo();
 });
-
-
 /**
     <h4>Valor a ser adicionado: <input id="soma"> </h4>
     <button id="atualizar-saldo">Atualizar saldo</button>
     <button id="limpar-saldo">Limpar seu saldo</button>
     <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
- */
+ */ 
+//# sourceMappingURL=desafio3.js.map
