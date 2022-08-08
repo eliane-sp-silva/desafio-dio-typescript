a3c952184534b73d045f182127c46dc00ebd4651
= <3lian3.pereira@gmail.com>
2022-08-07 22:05:56 -0300

    first commit

---
+++ src/desafio1.ts
@@ -0,0 +1,15 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+//Respostas
+let employee: {codeEmployee: Number, nameEmployee: String} = {
+  codeEmployee: 10,
+  nameEmployee: "John"
+};
+
+
+
+let funcionario: {code: number, name: string} = {
+  code: 20,
+  name: "eliane"
+}
+ 
---
+++ src/desafio2.ts
@@ -0,0 +1,27 @@
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1: {nome: string, idade: number, profissao: string} = {
+    nome: "maria",
+    idade: 29,
+    profissao: "atriz"
+};
+
+
+let pessoa2: {nome: string, idade: number, profissao: string} = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro"
+}
+
+
+let pessoa3: {nome: string, idade: number, profissao: string} = {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+};
+
+let pessoa4: {nome: string, idade: number, profissao: string} = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+}
